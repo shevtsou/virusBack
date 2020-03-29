584eb6f7263c9a22d7b8714e861a9e8ff2603ee1
test <test@test.com>
2020-03-29 15:24:52 +0300

    Views

--- start.js
+++ start.js
@@ -4,16 +4,40 @@ const cors = require('cors')
 const app = express()
 const port = 3001
 app.use(cors())
-let infected = 500
-let speed = 1 + 0.23 / (3600*24)
+
+const INITIAL_SPEED = 0.23
+
+let speed = 1 + INITIAL_SPEED / (3600*24)
+let visitors = 0
+let infected = 1
+
+function calcInfected(days) {
+    while (days > 0) {
+        days--;
+        infected *= 1 + INITIAL_SPEED
+    }
+}
+calcInfected(30)
 
 setInterval(() => {
     infected *= speed
 }, 1000)
+
 app.get('/count', (req, res) => {
+    console.log(infected)
     res.send({
-        infected: infected
+        infected: infected,
+        visitors,
+        chance: (speed - 1) * infected * (3600*24) / 9000000
     })
 })
+
+app.get('/visit', (req, res) => {
+    visitors++;
+    res.send({
+        status: "ok"
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
